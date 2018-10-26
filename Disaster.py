6e814d03d1df0c303351f550b9b8be695547be74
Ashwina Kumar <kumarashwina31@gmail.com>
2018-10-26 20:14:37 +0530

    Add files via upload

---
+++ Disaster.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+df = pd.read_csv("C:/Users/ashwinak/Desktop/flood_dataset.csv")
+df.head()
+from sklearn import linear_model
+logreg = linear_model.LogisticRegression()
+X= df[["PLACEID","MONTH"]]
+y=df[["TARGET(2018_month)"]]
+logreg.fit(X,y)
+#print(logreg.coef_)
+#print(logreg.intercept_)
+result = logreg.predict_proba([[1,10]])
+t = result[0][1]
+t
